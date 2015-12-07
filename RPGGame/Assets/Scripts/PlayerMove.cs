2df153c1c33522cee140aa75f3270ea01b1aa472
raulgrogovich <cray.jaeger1@gmail.com>
2015-12-07 01:41:52 -0600

    Created Multiplayer Demo

---
+++ RPGGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+    float speed = 10;
+    public GameObject cameraGameObject;
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            cameraGameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+    }
+}
